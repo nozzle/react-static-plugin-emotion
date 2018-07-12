e5c142db303a7d0c54749eba5ba08ad0e37ab6cc
Tanner Linsley <tannerlinsley@gmail.com>
2018-07-12 14:38:05 -0600

    v1.0.4

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const { renderStylesToString } = require("emotion-server");
 
 module.exports = {
-  default: {
-    // Tap into the HTML hook and use emotion-server
-    // to critically inline the styles from the original
-    // html into the a new html string
-    html: html => renderStylesToString(html)
-  }
+  // Tap into the HTML hook and use emotion-server
+  // to critically inline the styles from the original
+  // html into the a new html string
+  html: html => renderStylesToString(html)
 };
